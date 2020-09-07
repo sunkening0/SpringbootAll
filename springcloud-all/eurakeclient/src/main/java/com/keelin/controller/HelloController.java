308c65d9e836d5e0f17f420709af4a00fd9924bf
skn <812610493@qq.com>
2020-09-08 00:27:15 +0800

    springcloud整合eurake

---
+++ springcloud-all/eurakeclient/src/main/java/com/keelin/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.keelin.controller;
+
+import com.keelin.config.ServerConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    ServerConfig serverConfig;
+
+    @RequestMapping("hello")
+    public String hello(){
+        return serverConfig.getUrl()+"###"+ HelloController.class.getName();
+    }
+}
