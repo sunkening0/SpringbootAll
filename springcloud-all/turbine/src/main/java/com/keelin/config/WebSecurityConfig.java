aae814fea1301fa5f9da0605294fd75e760d269a
sunkening <812610493@qq.com>
2020-09-08 18:25:40 +0800

    springcloud整合turbine

---
+++ springcloud-all/turbine/src/main/java/com/keelin/config/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.keelin.config;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Order(0)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //为了方便  放开所有请求
+        http.authorizeRequests().anyRequest().permitAll()
+                .and().csrf().disable();
+    }
+}
