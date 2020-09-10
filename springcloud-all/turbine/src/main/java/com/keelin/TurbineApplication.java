409538a4cf96becfe153de0578cfec88eb5fc9c1
sunkening <812610493@qq.com>
2020-09-10 17:57:27 +0800

    turbine

--- springcloud-all/turbine/src/main/java/com/keelin/TurbineApplication.java
+++ springcloud-all/turbine/src/main/java/com/keelin/TurbineApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableTurbine
+@EnableTurbineStream   //启用Turbine Stream的配置
 public class TurbineApplication {
     public static void main(String[] args) {
         SpringApplication.run(TurbineApplication.class,args);
