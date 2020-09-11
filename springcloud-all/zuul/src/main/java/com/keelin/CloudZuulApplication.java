c92cec66616a2eeef4afac79c633fec7be505c9f
sunkening <812610493@qq.com>
2020-09-11 10:51:38 +0800

    zuul

--- springcloud-all/zuul/src/main/java/com/keelin/CloudZuulApplication.java
+++ springcloud-all/zuul/src/main/java/com/keelin/CloudZuulApplication.java
@@ -7,6 +7,9 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * @description: zuul网关
+ * Zuul的主要功能是路由转发和过滤器。路由功能是微服务的一部分，
+ * 比如／api/user转发到到user服务，/api/shop转发到到shop服务。
+ * zuul默认和Ribbon结合实现了负载均衡的功能。
  * @author: skn
  * @create: 2020-09-10 17:43
  */
