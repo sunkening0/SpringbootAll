e7beceb1428c30a5744159a3fe83f9a79004e6fc
sunkening <812610493@qq.com>
2020-09-30 18:47:35 +0800

    bus消息总线测试

--- springcloud-all/feign-client/src/main/java/com/keelin/service/FeignRemoteService.java
+++ springcloud-all/feign-client/src/main/java/com/keelin/service/FeignRemoteService.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
 /**
  * Feign 采用的是基于接口的注解
  * Feign 整合了ribbon
