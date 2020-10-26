dba9c2decd051cc2518b324468e8d86edf81ce6a
sunkening <812610493@qq.com>
2020-10-26 22:19:39 +0800

    no message

--- springbt-security/src/main/java/com/keelin/SecurityConfig.java
+++ springbt-security/src/main/java/com/keelin/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.keelin;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
